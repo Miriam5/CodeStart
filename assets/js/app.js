0eeae1444d5765323edf92176522aef5495efc8f
miriam <miriam5000@gmail.com>
2017-08-10 11:38:47 -0400

    first commit, move off codepen, remove w3css

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+function toggleBasics() {
+    var x = document.getElementById("buttonBasics");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function toggleBasics1() {
+    var x = document.getElementById("adultsTeach");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
